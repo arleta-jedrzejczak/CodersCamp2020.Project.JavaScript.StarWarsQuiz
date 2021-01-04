baf5c26a49ae5405096a8393c24d7d4c8f4f75e2
Arleta Jędrzejczak <laililang@gmail.com>
2021-01-04 02:07:05 +0100

    Create template for new components

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,8 @@
-export const App = ({options}) => {
+import Timer from './components/timer/timer.component';
+class App {
+    constructor(options) {
+        this.timer = new Timer('timer');
+    }
 }
 
-
+export default App
