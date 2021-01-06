52af0bae142bf34072324dfb4e083c72a13cd25d
Stanislaw09 <sgardzielewski09@gmail.com>
2021-01-06 23:07:04 +0100

    added rules module

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,7 @@
+import Rules from '../rules/rules'
+
 export const App = ({options}) => {
+      this.rules = new Rules('starships-intro')   // characters-intro | vehicles-intro | starships-intro | characters-question | vehicles-question | starships-question
 }
 
 
