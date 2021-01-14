38309190c97ff52f92fab86c86e737430d2934ba
Sebastian Jajesnica <sebastian.jajesnica@gmail.com>
2021-01-14 21:35:11 +0100

    v2

---
+++ src/images/images.js
@@ -0,0 +1,18 @@
+
+// types: People, Vehicles, Starships
+creatingButtonContainer (type)  {
+    const imgContainer = document.createElement('div');
+    mainMenuContainer.id = `img${type}`;
+    mainMenuContainer.style=
+        `
+        background-color: #fff; 
+        margin = 1rem; 
+        border-radius: 2rem; 
+        box-shadow: 0 0 25px 1px red;
+        display: flex;
+        justify-content: space-around;
+        padding: 4rem;
+        margin-bottom: 10rem;
+        `
+    document.querySelector('.logoImgSettings').appendChild(mainMenuContainer);
+};
