9d0dd9ee497ac41f509424ce4172868bcbeb9ea1
Sebastian Jajesnica <sebastian.jajesnica@gmail.com>
2021-01-14 22:58:10 +0100

    first version of script images.js

--- src/images/images.js
+++ src/images/images.js
@@ -1,18 +1,32 @@
 
 // types: People, Vehicles, Starships
-function creatingButtonContainer (type) {
-    const imgContainer = document.createElement('div');
-    mainMenuContainer.id = `img${type}`;
-    mainMenuContainer.style=
-        `
-        background-color: #fff; 
-        margin = 1rem; 
-        border-radius: 2rem; 
-        box-shadow: 0 0 25px 1px red;
-        display: flex;
-        justify-content: space-around;
-        padding: 4rem;
-        margin-bottom: 10rem;
-        `
-    document.querySelector('.logoImgSettings').appendChild(mainMenuContainer);
-};
+// function creatingImgContainer () {
+//     const imgContainer = document.createElement('div');
+//     imgContainer.id = `img`;
+//     imgContainer.style=
+//         ` 
+//         margin-bottom: 10rem;
+//         `
+//     document.querySelector('.logoImgSettings').appendChild(imgContainer);
+// };
+
+function addImgPeople (src) {
+    
+    const img = document.createElement('img');
+    let source = "./static/assets/img/modes/people/10.jpg";
+    // source = src
+    img.setAttribute("src", source);
+    img.style=
+    `
+    box-shadow: 0 0 25px 1px red;
+    border-radius: 2rem;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 70%;
+    width: auto;
+    `
+    
+    document.querySelector('.logoImgSettings').appendChild(img);
+}
+
+addImgPeople();
