e7bdd866c9d556f1e8b54313b67e5f54cb1e0d38
Raczysty <seba.raczkowski@gmail.com>
2021-01-18 22:14:35 +0100

    workflow commit

--- src/app/components/GameHanding.js
+++ src/app/components/GameHanding.js
@@ -4,17 +4,13 @@ class EventsHandlers {
         const answers = document.querySelectorAll('.answer');
 
         if (answers) {
-            console.log(answers);
             answers.from(el  => {
-                console.log(el);
                 el.addEventListener('click', ev => {
                     ev.preventDefault();
         
                     console.log(el);
                 });
             });
-        } else {
-            console.log('nope');
         }
     }
 }
