9e41f3fccc62a1ff8e5d0cf70c25b6e7ea551c67
UlaWilk <coyot.szafir@gmail.com>
2021-01-18 11:21:07 +0100

    clean the code, delete old code

--- src/app/App.js
+++ src/app/App.js
@@ -20,16 +20,3 @@ let players = [
 }
 
 export default App;
-
-/*
-export const App = ({options}) => {
-    
-    this.answearDisplay = new answearDisplay(answear, elementId);
-    // First argument 'answear' boolean, is responsible for checking correct answear
-    // Second is ID of a DOMelement which should be colored
-
-    this.rules = new Rules('starships-intro');   // characters-intro | vehicles-intro | starships-intro | characters-question | vehicles-question | starships-question
-    this.tableWithScores = new TableWithScores('table');
-    tableWithScores.renderTable(players);
-}
-*/
